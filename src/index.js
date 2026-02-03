b80540d350148914627747de524dfaa87ead0eb7
IGE SUNDAY ISRAEL <56985480+sunnyaquostic@users.noreply.github.com>
2026-02-03 18:36:33 +0100

    feat: add entry page src/index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express()
+const PORT = 8005;
+
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello from express server')
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`)
+})
